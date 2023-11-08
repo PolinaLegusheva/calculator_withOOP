cdae0f795413ab09d0a44df8b6e551b03525721a
PolinaLegusheva <polina.legusheva.003@mail.ru>
2023-11-08 14:03:36 +0300

    added cycles and file calculator.py

---
+++ calculator.py
@@ -0,0 +1,5 @@
+from main import Calculator
+
+if __name__ == "__main__":
+    calculator = Calculator()
+    calculator.mainloop()
--- main.py
+++ main.py
@@ -10,16 +10,9 @@ class Calculator(tk.Tk):
         self.display = ttk.Entry(self, width=50)
         self.display.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
-        button1 = Button(self, text="1", padx=60, pady=20, command=lambda: self.button_click(1))
-        button2 = Button(self, text="2", padx=60, pady=20, command=lambda: self.button_click(2))
-        button3 = Button(self, text="3", padx=60, pady=20, command=lambda: self.button_click(3))
-        button4 = Button(self, text="4", padx=60, pady=20, command=lambda: self.button_click(4))
-        button5 = Button(self, text="5", padx=60, pady=20, command=lambda: self.button_click(5))
-        button6 = Button(self, text="6", padx=60, pady=20, command=lambda: self.button_click(6))
-        button7 = Button(self, text="7", padx=60, pady=20, command=lambda: self.button_click(7))
-        button8 = Button(self, text="8", padx=60, pady=20, command=lambda: self.button_click(8))
-        button9 = Button(self, text="9", padx=60, pady=20, command=lambda: self.button_click(9))
-        button0 = Button(self, text="0", padx=60, pady=20, command=lambda: self.button_click(0))
+        for i in range(0, 10):
+            button = Button(self, text=str(i), padx=60, pady=20, command=lambda x=i: self.button_click(x))
+            button.grid(row=i//3+1, column=i%3)
 
         button_add = Button(self, text="+", padx=58, pady=20, command=self.click_addition)
         button_subtract = Button(self, text="-", padx=61, pady=20, command=self.click_subtraction)
@@ -29,17 +22,6 @@ class Calculator(tk.Tk):
         button_root = Button(self, text="√", padx=60, pady=20, command=self.click_root)
         button_clear = Button(self, text="Clear", padx=47, pady=20, command=self.click_clearing)
         button_equal = Button(self, text="=", padx=58, pady=20, command=self.calculation)
-
-        button1.grid(row=3, column=0)
-        button2.grid(row=3, column=1)
-        button3.grid(row=3, column=2)
-        button4.grid(row=2, column=0)
-        button5.grid(row=2, column=1)
-        button6.grid(row=2, column=2)
-        button7.grid(row=1, column=0)
-        button8.grid(row=1, column=1)
-        button9.grid(row=1, column=2)
-        button0.grid(row=4, column=0)
 
         button_add.grid(row=5, column=0)
         button_subtract.grid(row=6, column=0)
@@ -128,6 +110,3 @@ class Calculator(tk.Tk):
             self.operation = None
             self.result = None
 
-if __name__ == "__main__":
-    calculator = Calculator()
-    calculator.mainloop()
